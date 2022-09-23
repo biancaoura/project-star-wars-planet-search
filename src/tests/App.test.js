058304e836d175a721309e82d589ab61890cb0a1
Bianca Oura <biancaoura@gmail.com>
2022-09-22 23:43:58 -0300

    Add testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,7 +5,7 @@ import App from '../App';
 import userEvent from '@testing-library/user-event';
 import mockAPI from './helpers/mockAPI';
 
-describe('Testing NumericFilters component', () => {
+describe('Testing resolved promise', () => {
   beforeEach(async () => {
     jest.spyOn(global, 'fetch');
     global.fetch = jest.fn(() => Promise.resolve({
@@ -34,4 +34,54 @@ describe('Testing NumericFilters component', () => {
     expect(planetName[0]).toHaveTextContent(/naboo/i);
   });
 
+  it('3 - Clicking the button to remove an individual filter should render an updated list', async () => {
+    const planetName = await screen.findAllByRole('cell');
+    expect(planetName[0]).toHaveTextContent(/tatooine/i);
+
+    const columnInput = screen.getByRole('combobox', { name: /coluna/i });
+    const comparisonInput = screen.getByRole('combobox', { name: /operador/i });
+    const valueInput = screen.getByRole('spinbutton', { name: /valor/i });
+    const filterBtn = screen.getByRole('button', { name: /filtrar/i });
+
+    userEvent.selectOptions(columnInput, 'diameter');
+    userEvent.selectOptions(comparisonInput, 'menor que');
+    userEvent.type(valueInput, '10000');    
+    userEvent.click(filterBtn);
+
+    expect(planetName[0]).toHaveTextContent(/hoth/i);
+
+    const removeBtn = screen.getByRole('button', { name: /x/i });
+    
+    userEvent.click(removeBtn);
+    expect(planetName[0]).toHaveTextContent(/tatooine/i);
+    expect(removeBtn).not.toBeInTheDocument();
+  });
+
+  it('4 - Clicking the button to remove all filters should render the entire list of planets', async () => {
+    const planetName = await screen.findAllByRole('cell');
+    expect(planetName[0]).toHaveTextContent(/tatooine/i);
+
+    const columnInput = screen.getByRole('combobox', { name: /coluna/i });
+    const comparisonInput = screen.getByRole('combobox', { name: /operador/i });
+    const valueInput = screen.getByRole('spinbutton', { name: /valor/i });
+    const filterBtn = screen.getByRole('button', { name: /filtrar/i });
+
+    userEvent.selectOptions(columnInput, 'diameter');
+    userEvent.selectOptions(comparisonInput, 'menor que');
+    userEvent.type(valueInput, '10000');    
+    userEvent.click(filterBtn);
+
+    userEvent.selectOptions(columnInput, 'rotation_period');
+    userEvent.selectOptions(comparisonInput, 'igual a');
+    userEvent.type(valueInput, '18');    
+    userEvent.click(filterBtn);
+
+    expect(planetName[0]).toHaveTextContent(/endor/i);
+
+    const removeAllBtn = screen.getByRole('button', { name: /remover todos/i });
+
+    userEvent.click(removeAllBtn);
+    expect(planetName[0]).toHaveTextContent(/tatooine/i);
+  });
 });
+
